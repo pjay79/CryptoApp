41cd5c9396263f1ffd25062aff93279957d899f7
pjay79 <praveen.jayarajan@gmail.com>
2018-02-21 23:31:03 +1100

    Format price

--- app/components/CryptoItem/CryptoItem.js
+++ app/components/CryptoItem/CryptoItem.js
@@ -10,7 +10,9 @@ const CryptoItem = ({ crypto }) => (
       <Image style={styles.icon} source={Icons[crypto.symbol]} />
       <Text style={styles.symbol}>{crypto.symbol}</Text>
       <Text>{crypto.name}</Text>
-      <Text style={styles.price}>${crypto.price_usd}</Text>
+      <Text style={styles.price}>
+        $US{new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(crypto.price_usd)}
+      </Text>
     </View>
     <View style={styles.lowerContentStyle}>
       <Text style={styles.seven}>
